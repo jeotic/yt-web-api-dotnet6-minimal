91085685254ceaa39df041ef76aa178971a4be58
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2021-11-28 15:22:03 +0200

    Initial commit

---
+++ Models/User.cs
@@ -0,0 +1,8 @@
+namespace dotnet_6_minimal_wep_api.Models{
+    public class User{
+        public int Id { get; set; }
+        public string First_Name { get; set; }
+        public string Last_Name { get; set; }
+        public string Framework { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
+using dotnet_6_minimal_wep_api.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// Route
+app.MapGet("/api/v1/users", () => {
+    var list = new List<User>(1000);
+    for (int i = 1; i < 1001; i++) {
+        list.Add(new User {
+                Id = i,
+                First_Name = "First_Name" + i,
+                Last_Name = "Last_Name" + i,
+                Framework = "dotnet6"
+            }
+        );
+    }
+
+    return list;
+});
+
+// Run
+app.Run();

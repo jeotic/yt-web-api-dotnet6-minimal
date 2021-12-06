81894fc51345c87166802d75750ad873330c1f51
Mark Pflug <MarkPflug@users.noreply.github.com>
2021-12-06 12:22:56 -0800

    Use caching

--- Program.cs
+++ Program.cs
@@ -1,26 +1,25 @@
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Builder;
 using dotnet_6_minimal_wep_api.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-// Route
-app.MapGet("/api/v1/users", () => {
-    var list = new List<User>(1000);
-    for (int index = 1; index < 1001; index++) {
-        list.Add(new User {
-                Id = index,
-                Age = 25,
-                First_Name = "First_Name" + index,
-                Last_Name = "Last_Name" + index,
-                Framework = "dotnet6 minimala"
-            }
-        );
-    }
+var data = new List<User>(1000);
 
-    return list;
-});
+for (int index = 1; index < 1001; index++)
+{
+    data.Add(new User
+    {
+        Id = index,
+        Age = 25,
+        First_Name = "First_Name" + index,
+        Last_Name = "Last_Name" + index,
+        Framework = "dotnet6 minimala"
+    }
+    );
+}
+
+// Route
+app.MapGet("/api/v1/users", () => data);
 
 // Run
 app.Run();

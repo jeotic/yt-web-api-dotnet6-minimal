3861853218f26ccfa41af911ebb5c406f1b142f6
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2021-12-18 14:36:32 +0200

    Renaming variable

--- Program.cs
+++ Program.cs
@@ -1,25 +1,25 @@
+using System.Collections.Generic;
 using dotnet_6_minimal_wep_api.Models;
+using Microsoft.AspNetCore.Builder;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-var data = new List<User>(1000);
+var cachedUsers = new List<User>(1000);
 
-for (int index = 1; index < 1001; index++)
-{
-    data.Add(new User
-    {
-        Id = index,
-        Age = 25,
-        First_Name = "First_Name" + index,
-        Last_Name = "Last_Name" + index,
-        Framework = "dotnet6 minimala"
-    }
+for (int index = 1; index < 1001; index++) {
+    cachedUsers.Add(new User {
+            Id = index,
+            Age = 25,
+            First_Name = "First_Name" + index,
+            Last_Name = "Last_Name" + index,
+            Framework = "dotnet6 minimala"
+        }
     );
 }
 
 // Route
-app.MapGet("/api/v1/users", () => data);
+app.MapGet("/api/v1/users", () => cachedUsers);
 
 // Run
 app.Run();
